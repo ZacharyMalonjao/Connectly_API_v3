781aa18f56da6e3f647d6cdd9b8cf4963dd56492
ZacharyMalonjao <158264225+ZacharyMalonjao@users.noreply.github.com>
2026-02-05 00:21:36 +0800

    Security works, tokens and sessionids work

--- connectly_project/posts/models.py
+++ connectly_project/posts/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-from django.db import models
 
-class User(models.Model):
-    username = models.CharField(max_length=100, unique=True)  # User's unique username
-    email = models.EmailField(unique=True)  # User's unique email
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when the user was created
 
-    def __str__(self):
-        return self.username
+# class User(models.Model):
+#     username = models.CharField(max_length=100, unique=True)  # User's unique username
+#     email = models.EmailField(unique=True)  # User's unique email
+#     created_at = models.DateTimeField(auto_now_add=True)  # Timestamp when the user was created
+
+#     def __str__(self):
+#         return self.username
 
 
 class Post(models.Model):
